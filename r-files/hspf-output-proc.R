72cfea028b4cda1e9635b9ebd601b4914d5268f5
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-28 11:08:34 -0800

    added baseflow index calculation

--- r-files/hspf-output-proc.R
+++ r-files/hspf-output-proc.R
@@ -4,6 +4,9 @@ source(file = paste0(chr.dir.r.files, "/functions.R"))
 chr.dir.hspf <- "m:/models/bacteria/hspf/bigelkhydrocal201601/hspf-files"
 chr.file.hspf.out <- "behydcal.out"
 
+## drainage area in sqr mi for Big Elk Creek at outlet
+da.be <- 88.8
+
 df.mod <- rpltgen(chr.dir = chr.dir.hspf, chr.file = chr.file.hspf.out)
 
 ## calculate model groups
@@ -13,13 +16,24 @@ mlog <- log10(df.mod$Rch18.flow)
 ## mflow stream flow
 mflow <- df.mod$Rch18.flow
 
-## mbaseind
+## mbaseind, base flow index 
+## baseflow seperation using USGS-HySep R version in DVstats
+require(DVstats)
+df.hysep88.8 <- hysep(Flow = df.mod$Rch18.flow , 
+                      Dates = as.Date(df.mod$tmp.date), da = da.be)
 
+## caclulate baseflow in the same way as calculated from the observed data
+mbaseind <- sum(df.hysep88.8$BaseQ)/sum(df.hysep88.8$Flow)
 
-## mpeak
+## clean up
+rm(df.hysep88.8)
+
 ## mvol_ann
 ## mvol_smr
 ## mvol_wtr
+
+## mpeak
 ## mvol_stm
+
 ## mtime
 
