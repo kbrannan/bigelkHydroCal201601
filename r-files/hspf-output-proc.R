1f7aee4628eac31e11b4eb223271009404907a5a
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-28 09:47:00 -0800

    adding code to calculate the observation/model groups for comparison. added mlog amd mflow

--- r-files/hspf-output-proc.R
+++ r-files/hspf-output-proc.R
@@ -4,4 +4,22 @@ source(file = paste0(chr.dir.r.files, "/functions.R"))
 chr.dir.hspf <- "m:/models/bacteria/hspf/bigelkhydrocal201601/hspf-files"
 chr.file.hspf.out <- "behydcal.out"
 
-junk <- rpltgen(chr.dir = chr.dir.hspf, chr.file = chr.file.hspf.out)
+df.mod <- rpltgen(chr.dir = chr.dir.hspf, chr.file = chr.file.hspf.out)
+
+## calculate model groups
+## mlog log base 10 of stream flow
+mlog <- log10(df.mod$Rch18.flow)
+
+## mflow stream flow
+mflow <- df.mod$Rch18.flow
+
+## mbaseind
+
+
+## mpeak
+## mvol_ann
+## mvol_smr
+## mvol_wtr
+## mvol_stm
+## mtime
+
