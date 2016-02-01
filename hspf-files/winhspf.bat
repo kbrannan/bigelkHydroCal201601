4dd1721abc0981f01346a451738ab9b066737ae4
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-01 15:12:10 -0800

    using full paths when calling input files

--- hspf-files/winhspf.bat
+++ hspf-files/winhspf.bat
@@ -1,1 +1,4 @@
+m:
+cd Models\Bacteria\HSPF\bigelkHydroCal201601\hspf-files
+del M:\Models\Bacteria\HSPF\bigelkHydroCal201601\hspf-files\behydcal.out
 C:\BASINS41\models\HSPF\bin\WinHspfLt.exe -1 -1 M:\Models\Bacteria\HSPF\bigelkHydroCal201601\hspf-files\bigelk.uci
--- r-files/post-proc.bat
+++ r-files/post-proc.bat
@@ -1,1 +1,3 @@
+del m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\model.ins
+del m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\model.out
 "C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\hspf-output-proc.R
