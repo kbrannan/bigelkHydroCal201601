054ce3ac963ace15c23676908f1b9377c9201fa2
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-03 14:16:55 -0800

    set paths for model.out in control.pst and put par2par call in model.bat

--- pest-files/model.bat
+++ pest-files/model.bat
@@ -1,4 +1,5 @@
 @echo off
 rem del %~dp0model.out
+par2par p2p_bigelk.dat
 call m:\models\bacteria\hspf\bigelkhydrocal201601\hspf-files\winhspf.bat
 call m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\post-proc.bat
