5c1306b9e736c07cad53ff776209957c03ff2a8d
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-03 10:43:23 -0800

    trying to track down message that cannot find model.out I don't think it is effecting the pest run, but I want to address it

--- pest-files/model.bat
+++ pest-files/model.bat
@@ -1,3 +1,4 @@
 @echo off
+del %~dp0model.out
 call m:\models\bacteria\hspf\bigelkhydrocal201601\hspf-files\winhspf.bat
 call m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\post-proc.bat
--- r-files/post-proc.bat
+++ r-files/post-proc.bat
@@ -1,4 +1,4 @@
 echo off
-del model.ins
-del model.out
+rem del model.ins
+rem del model.out
 "C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla %~dp0hspf-output-proc.R
