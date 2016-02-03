dbde0636dee9ccded2e7e75a4a3ad18a79f6b878
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-03 15:05:47 -0800

    sent output > nul for programs and pat files in model.bat

--- pest-files/model.bat
+++ pest-files/model.bat
@@ -1,5 +1,5 @@
 @echo off
 rem del %~dp0model.out
-par2par p2p_bigelk.dat
-call m:\models\bacteria\hspf\bigelkhydrocal201601\hspf-files\winhspf.bat
-call m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\post-proc.bat
+par2par p2p_bigelk.dat > nul
+call m:\models\bacteria\hspf\bigelkhydrocal201601\hspf-files\winhspf.bat > nul
+call m:\models\bacteria\hspf\bigelkhydrocal201601\r-files\post-proc.bat > nul
