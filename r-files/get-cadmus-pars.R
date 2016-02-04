22ed722bfa6586ac5c3ad5b6a0f445cfbfad7983
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-04 12:15:55 -0800

    inserted calibrated parmeters into the control file. I had to manually chenge the pst file to get all the columns in the parameter data block. I will code these chenges after I run PEST

--- r-files/get-cadmus-pars.R
+++ r-files/get-cadmus-pars.R
@@ -2,11 +2,11 @@
 ## the current PEST control.file
 
 # zipfile path and name
-tmp.dir <- "//deqhq1/tmdl/TMDL_WR/MidCoast/Models/Bacteria/HSPF/Hydro Calibration/Files from Cadmus"
+tmp.zip.dir <- "//deqhq1/tmdl/TMDL_WR/MidCoast/Models/Bacteria/HSPF/Hydro Calibration/Files from Cadmus"
 tmp.zip <- "Final_Deliverables_EPA_July2012.zip"
 
 # get list of files in the zipfile
-tmp.fns <- unzip(zipfile = paste0(tmp.dir,"/", tmp.zip), list = TRUE)
+tmp.fns <- unzip(zipfile = paste0(tmp.zip.dir,"/", tmp.zip), list = TRUE)
 
 # see how many occurances there are of the control file in the zipfile
 tmp.par <- grep("*\\.par", tmp.fns$Name, value = TRUE)
@@ -20,3 +20,28 @@ str.par <- scan(unz(paste0(tmp.dir,"/", tmp.zip),
                     tmp.file)
                 , sep = "\n", what = character())
 str.par
+
+# get current control file
+tmp.pst.dir <- "//deqhq1/tmdl/TMDL_WR/MidCoast/Models/Bacteria/HSPF/bigelkHydroCal201601/pest-files"
+str.cur.pst <- scan(paste0(tmp.pst.dir,"/control.pst"), sep = "\n", what = character())
+
+grep("\\*", str.cur.pst, value = TRUE)
+
+## insert block of calib pars into the control
+lng.cur.st <- grep("\\* parameter data" ,str.cur.pst)
+lng.cur.ed <- lng.cur.st + min(grep("\\* " , 
+                                    str.cur.pst[(lng.cur.st + 1):length(str.cur.pst)]))
+
+str.cur.pst[lng.cur.st:lng.cur.ed]
+
+str.new.pst <- c(str.cur.pst[1:lng.cur.st], 
+                     str.par[2:length(str.par)],
+                     str.cur.pst[lng.cur.ed:length(str.cur.pst)])
+
+str.new.pst[lng.cur.st:lng.cur.ed]
+
+write.table(str.new.pst, file = paste0(tmp.pst.dir,"/control-cadmus.pst"), 
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+
+
