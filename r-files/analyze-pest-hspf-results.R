f1a1c8f8ddddf8c0e02fc1ec5d65b3bddddfbb2e
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-09 14:55:20 -0800

    create data frame of residual information

--- r-files/analyze-pest-hspf-results.R
+++ r-files/analyze-pest-hspf-results.R
@@ -6,14 +6,23 @@ chr.dir <- "M:/Models/Bacteria/HSPF/bigelkHydroCal201601"
 ## read residuals file
 chr.res <- scan(file = paste0(chr.dir,"/pest-files/control.res"),
                 what = "character", sep = "\n")
-head(chr.res)
-
+## replace * with x in field names
 chr.res[1] <- gsub("\\*","x",chr.res[1])
 
+## replace spaces among columns with comma
 chr.res <- gsub("( ){1,}",",",chr.res)
 
-df.res <- data.frame(do.call(rbind,strsplit(chr.res,split = ",")))[ , -1]
+## convert chracter vector to data.frame
+df.res <- data.frame(do.call(rbind,strsplit(chr.res,split = ",")), 
+                     stringsAsFactors = FALSE)
 
+## remove first column becuase it is empty
+df.res <- df.res[ ,-1]
+
+## first row is names for columns
 names(df.res) <- df.res[1, ]
 
-df.res <- df.res[ ,-1]
+## discard first row
+df.res <- df.res[-1, ]
+
+head(df.res)
