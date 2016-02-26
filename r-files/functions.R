352d33f38dcd76f59a15b83985c7b20f3c54d384
kbrannan <brannan.kevin@deq.state.or.us>
2016-02-25 16:23:26 -0800

    creating plot_storm function that uses ggplot

--- r-files/functions.R
+++ r-files/functions.R
@@ -287,8 +287,8 @@ storms_plot_to_list <- function(dte.stms = dte.stms,
 #       precip     <- df.daily.precip$daily.precip
 
   
-  dev.new(noRStudioGD = FALSE)
-  
+  ##dev.new(noRStudioGD = FALSE)
+
   ## create list to put plots in
   my.plots <- vector(length(dte.stms$begin), mode='list')
   
@@ -398,6 +398,243 @@ storms_plot_to_list <- function(dte.stms = dte.stms,
     ## reset graphics
     ##graphics.off()
   }
-  dev.off()
+##  dev.off()
+return(my.plots)
+}
+
+storms_plot <- function(lng.stm,
+                        dte.stms = dte.stms,
+                        dte.flows  = dte.flows,
+                        obs.flow   = obs.flow,
+                        mod.flow   = mod.flow,
+                        obs.bflow  = obs.bflow,
+                        mod.bflow  = mod.bflow,
+                        storms.peak  = storms.peak,
+                        storms.vol = storms.vol,
+                        precip     = precip) {
+  
+  # plot flow and precip information for each individul storm and store
+  # in a list
+  # input:
+  #
+  #     lng.stm - storm number (row number in storm info data.frames)
+  #    dte.stms - data.frame of POSIXct dates that correspond to first and last 
+  #               days of storms
+  #   dte.flows - vector of POSIXct dates that correspond to flow values
+  #    obs.flow - vector of observed daily flow values in cfs
+  #    mod.flow - vector of modeled daily flow values in cfs
+  #   obs.bflow - vector of observed daily base flow values in cfs
+  # storms.peak - data.frame for peak flow data for each storm
+  #  storms.vol - data.frame for flow volume data for each storm
+  #      precip - vector of numeric precip values in inches
+  #    out.file - string for pdf filename (and path) where figure is sent
+  #
+  # output:
+  # p.stm <- plot of storm (ggplot object)
+  
+    
+  # temporary assignments for function development
+  dte.stms <- df.storms.peak[ , c("begin", "end")]
+  dte.flows  <- df.mflow$dates
+  obs.flow   <- df.mflow$Measured
+  mod.flow   <- df.mflow$Modelled
+  obs.bflow  <- df.hysep88.8.obs$BaseQ
+  mod.bflow  <- df.hysep88.8.mod$BaseQ
+  storms.peak <- df.storms.peak
+  storms.vol <- df.storms.vol
+  obs.pflow  <- storms.peak$Measured
+  mod.pflow  <- storms.peak$Modelled
+  obs.vflow  <- storms.vol$Measured
+  mod.vflow  <- storms.vol$Modelled
+  precip     <- df.daily.precip$daily.precip
+
+  lng.stm <- 4
+
+  
+  ## load pakage
+  require(ggplot2, quietly = TRUE)
+
+  # title for plot is current storm
+  tmp.title = paste0("storm num ", lng.stm,
+                     "  Season = ", storms.peak$season[lng.stm],
+                     "\nObs Flow-zone = ", storms.peak$obs.flw.zn[lng.stm],
+                     "  Mod Flow-zone = ", storms.peak$mod.flw.zn[lng.stm],
+                     "\nObs peak flow = ", sprintf(fmt = "%-.2f cfs", storms.peak$Measured[lng.stm]),
+                     " Mod peak flow = ", sprintf(fmt = "%-.2f cfs", storms.peak$Modelled[lng.stm]),
+                     "  Obs storm vol = ", sprintf(fmt = "%-.2E ac-ft", storms.vol$Measured[lng.stm]),
+                     " Mod storm vol = ", sprintf(fmt = "%-.2E ac-ft", storms.vol$Modelled[lng.stm]))
+
+  ## get data for storm plot  
+  ## begin row for storm in flow time-series
+  lng.begin <- grep(strftime(dte.stms$begin[lng.stm], format = "%Y%m%d"), 
+                    strftime(dte.flows, format = "%Y%m%d"))
+  lng.end   <- grep(strftime(dte.stms$end[lng.stm], format = "%Y%m%d"), 
+                    strftime(dte.flows, format = "%Y%m%d"))
+  if(length(lng.begin) > 1 | length(lng.end) > 1) print(paste0("Too many matches for dates for storm ", lng.stm))
+  ## expand range for plotting
+  lng.ex <- 2 # number of days before start and after end
+  lng.begin.ex <- max(lng.begin - lng.ex, 1)
+  lng.end.ex   <- min(lng.end + lng.ex, length(dte.flows))
+  ## dates for storm time series
+  tmp.dte.stm.ts <- dte.flows[lng.begin:lng.end]
+  # get data for current storm
+  tmp.dates  <- dte.flows[lng.begin.ex:lng.end.ex]
+  tmp.data <- rbind(
+    data.frame(date = tmp.dates,
+               value = df.daily.precip$daily.precip[lng.begin.ex:lng.end.ex],
+               sub.group = "precip",
+               group = "precip",
+               src = "obs"),
+    data.frame(date = tmp.dates,
+               value = obs.flow[lng.begin.ex:lng.end.ex],
+               sub.group = "obs-flow",
+               group = "flow",
+               src = "obs"),
+    data.frame(date = tmp.dates,
+               value = mod.flow[lng.begin.ex:lng.end.ex],
+               sub.group = "mod-flow",
+               group = "flow",
+               src = "mod"),
+    data.frame(date = tmp.dates,
+               value = obs.bflow[lng.begin.ex:lng.end.ex],
+               sub.group = "obs-baseflow",
+               group = "flow",
+               src = "obs"),
+    data.frame(date = tmp.dates,
+               value = mod.bflow[lng.begin.ex:lng.end.ex],
+               sub.group = "mod-baseflow",
+               group = "flow",
+               src = "mod"),
+    data.frame(date = min(tmp.dte.stm.ts[df.mflow$Measured[lng.begin:lng.end] == storms.peak$Measured[lng.stm]]),
+               value = storms.peak$Measured[lng.stm],
+               sub.group = "obs-peak-flow",
+               group = "flow",
+               src = "obs"),
+    data.frame(date = min(tmp.dte.stm.ts[df.mflow$Modelled[lng.begin:lng.end] == storms.peak$Modelled[lng.stm]]),
+               value = storms.peak$Modelled[lng.stm],
+               sub.group = "mod-peak-flow",
+               group = "flow",
+               src = "mod"))
+
+tmp.data$sub.group <- factor(tmp.data$sub.group, 
+                         levels = c("precip", "obs-flow","mod-flow",
+                         "obs-baseflow", "mod-baseflow",
+                         "obs-peak-flow", "mod-peak-flow"))
+tmp.data$group <- factor(tmp.data$group, 
+                             levels = c("precip", "flow"))
+tmp.data$src <- factor(tmp.data$src, 
+                         levels = c("obs", "mod"))
+
+  
+  # calculate pars for plot
+  # set y-limits for storm
+  tmp.ylims <- 
+    c(10 ^ 
+        (
+          floor(
+            log10(
+              min(
+                tmp.data$value[with(tmp.data, group == "flow")])))),
+      10 ^ (
+        ceiling(
+          log10(
+            max(tmp.data$value[with(tmp.data, group == "flow")])))))
+  # set x-limits for current storm
+  tmp.xlims <- c(dte.flows[lng.begin.ex], dte.flows[lng.end.ex])
+
+  
+  
+    
+  # precip plot
+  p.precip <- ggplot(data = tmp.data[tmp.data$sub.group == "precip", ],
+                         aes(x = date, y = value)) +
+    xlim(tmp.xlims) + 
+    ylim(c(0, max(c(tmp.data$value[tmp.data$group == "precip"], 0.1)))) +
+    ylab("precip in") +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
+  # plot vertical lines for each precip obs
+  p.precip <- p.precip + 
+    geom_segment(aes(xend = date, 
+                     y = rep(0, length(tmp.dte.flows)), yend=value))
+
+  
+ ## flow plot
+  tmp.flow <- tmp.data[tmp.data$group == "flow", ]
+
+  p.flow <- ggplot(data = tmp.flow, 
+                   aes(x = date, y = value, color = sub.group))
+  # add obs flow
+  p.flow <- p.flow + 
+    geom_line() + geom_point(aes(size = c(0.1, 0.1, 0.1, 0.1, 2.5, 2.5))) +
+    scale_colour_manual(name = "",
+                        labels = unique(as.character(tmp.flow$sub.group)),
+                        values = rep(c("blue","red"),3)) + 
+    scale_shape_manual(name = "",
+                        labels = unique(as.character(tmp.flow$sub.group)),
+                        values = rep(c(19,25),3)) +
+    scale_size_manual(name = "",
+                       labels = unique(as.character(tmp.flow$sub.group)),
+                       values = c(0, 0, 0, 0, 2.5, 2.5))
+    
+  
+  
+  plot(p.flow)
+  
+  p.flow <- p.flow +
+    guides(
+      shape = guide_legend(override.aes = list(shape = c(NA, NA, NA, NA, 19, 17))))
+
+  ## format legend
+  p.flow <- p.flow +
+    scale_colour_manual(name = "",
+                        labels = levels(sub.group),
+                        values = src) +
+    scale_shape_manual(labels = levels(tmp.data$group)[-1],
+                       values = c(19, 17, 19, 17, 19, 17)) +
+    scale_size_manual(labels = levels(tmp.data$group)[-1],
+                      values = c(0, 0, 3, 0, 0, 3))
+  
+  
+  
+    plot(p.flow)
+  
+      # set up plot for flow, don't plot data. Plot flow data after the storm
+    # polygons are plotted so flow lines are borders of polygons  
+    plot(x = tmp.dte.flows, y = tmp.obs.flow, type = "l", log = "y", lty = "blank",
+         xlim = tmp.xlims, ylim = tmp.ylims, xaxt = "n")
+    
+    # storm flow as a filled in polygon
+    #polygon(x = tmp.dte.flows, y = tmp.obs.flow, col = "yellow", lty = "blank")
+    
+    # flow data storm polygon
+    lines(x = tmp.dte.flows, y = tmp.obs.flow, type = "l", col = "blue")
+    lines(x = tmp.dte.flows, y = tmp.mod.flow, type = "l", col = "red")
+    
+    # base flow data storm polygon
+    lines(x = tmp.dte.flows, y = tmp.obs.bflow, type = "l", lty = "dashed", col = "blue")
+    lines(x = tmp.dte.flows, y = tmp.mod.bflow, type = "l", lty = "dashed", col = "red")
+    
+    
+    # points for peaks ploted over storm polygon and flow data
+    points(x = tmp.dte.obs.pflow, y = storms.peak$Measured[ii], col = "blue", 
+           pch = 2, cex = 1.1)
+    points(x = tmp.dte.mod.pflow, y = storms.peak$Modelled[ii], col = "red", 
+           pch = 2, cex = 1.1)
+    
+    # add grid lines in plot for dates
+    grid(nx = 30, ny = NULL)
+    
+    # format x-axis on flow plot to include day, month and year
+    axis.Date(side = 1, x = tmp.dte.flows, format = "%m-%d-%Y")
+    
+    ## store current plot in list
+    my.plots[[ii]] <- recordPlot()
+    
+    ## reset graphics
+    ##graphics.off()
+  }
+  ##  dev.off()
   return(my.plots)
 }
